9bc11a515cffd01d81e7d82c63fcc190e249b31f
karen111-star <karenborda111@gmail.com>
2025-11-04 22:29:46 -0500

    wa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserList from './componentes/ListaUsuarios';
+import Login from './componentes/Login';
 
 function App() {
-return (
-<div>
-<h1>React + MySQL Example</h1>
+	const [user, setUser] = useState(null);
 
-<UserList />
-</div>
-);
+	function handleLogin(usuario) {
+		setUser(usuario);
+	}
+
+	function handleLogout() {
+		setUser(null);
+	}
+
+		return (
+			<div>
+				<h1>Inicio de sesión</h1>
+
+				{user ? (
+				<div style={{ padding: '20px' }}>
+					<p>
+						Bienvenido, <strong>{user.nombre}</strong> ({user.email}){' '}
+						<button onClick={handleLogout}>Cerrar sesión</button>
+					</p>
+					<UserList />
+				</div>
+			) : (
+				<Login onLogin={handleLogin} />
+			)}
+		</div>
+	);
 }
 
 export default App;
